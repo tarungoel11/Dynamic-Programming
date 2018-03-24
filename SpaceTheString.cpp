ea30c0937b8e43c333a4a1e268c231b6e3e8a66d
tarungoel11 <tarungoel11@yahoo.com>
2018-03-24 11:42:10 +0530

    Spacing a string

---
+++ SpaceTheString.cpp
@@ -0,0 +1,51 @@
+#include<string>
+#include<iostream>
+// properly space a string
+
+using namespace std;
+
+bool compareString(string const &str1, const char &str2)
+{
+	string temp(&str2, str1.length());
+	cout<<"comparing "<<str1<<" and "<<temp<<endl;
+	if(str1 == temp)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+	
+}
+
+bool SpaceTheString(string s, int start, int len, string dict[], int dictLen)
+{
+	if(len == 0)
+	{
+		cout<<"done \n";
+		return true;
+	}
+	for(int i = 0; i< dictLen; i++)
+	{
+		if(len>= dict[i].length())
+		{
+			if(true == compareString(dict[i], s[start]))
+			{
+				if(true ==SpaceTheString(s, start+dict[i].length(), len - dict[i].length(), dict, dictLen))
+				{
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+int main()
+{
+	string str = "thisisacrazyworld";
+	string dict[] = {"this", "is", "a", "crazy", "world"};
+	
+	SpaceTheString(str, 0, str.length(), dict, 5);
+}
